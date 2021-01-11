863a2e920beaad065431d0115a6c589dd22beffc
chaudhary6-1 <chaudzai@b-tu.de>
2021-01-11 14:50:06 +0100

    hamburger fixed

--- docs/js/hamburger.js
+++ docs/js/hamburger.js
@@ -1,28 +1,31 @@
-const navSlide = () =>{
+const navSlide = () => {
     const burger = document.querySelector('.hamburger-menu');
     const nav = document.querySelector('.menu-items');
     const navLinks = document.querySelectorAll('.menu-items a')
 
-    burger.addEventListener('click', ()=>{
+    burger.addEventListener('click', () => {
         //Toggle Nav
-       nav.classList.toggle('nav-active');
+        nav.classList.toggle('nav-active');
 
         //Animate Links
-        navLinks.forEach((link, index)=>{
-            if(link.style.animation) {
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
                 link.style.animation = ''
-            }
-            else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index/4 + 0.5}s`;
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 4 + 0.5}s`;
             }
         });
 
         //Burger Animation
         burger.classList.toggle('toggle');
+
+        //position to fixed
+        nav.classList.toggle('nav-position');
+
     });
 }
 
-const app = ()=>{
+const app = () => {
     navSlide();
 }
 
